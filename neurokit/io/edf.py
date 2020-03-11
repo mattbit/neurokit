87d624aeaa52cbc3a21e62e57a9e4b7eb5fb29f3
Matteo Dora <matteo.dora@ens.fr>
2020-03-11 11:24:38 +0100

    Remove duplicate code

--- neurokit/io/edf.py
+++ neurokit/io/edf.py
@@ -1,50 +1,18 @@
-import dateparser
-import unidecode
-import chardet
-import shutil
 import re
 import mne
+import shutil
+import chardet
 import logging
+import unidecode
+import dateparser
 import numpy as np
 import pandas as pd
+from pathlib import Path
 from fractions import Fraction
 from pyedflib import EdfWriter
-from pathlib import Path
 
 from .model import Recording
-
-
-def _recording_from_mne_raw(raw: mne.io.Raw):
-    mne.utils.logger.setLevel(logging.WARNING)
-
-    timestamp, microseconds = raw.info['meas_date']
-    initial_time = timestamp * 10**9 + microseconds * 10**3
-
-    data = raw.to_data_frame(scaling_time=10**9)
-
-    data.index = pd.to_datetime(initial_time + data.index, unit='ns')
-
-    meta = {
-        'frequency': raw.info['sfreq'],
-        'resolution': [ch['range'] / ch['cal'] for ch in raw.info['chs']],
-    }
-
-    # Annotations
-    annot_list = []
-    for annotation in raw.annotations:
-        onset = pd.Timedelta(seconds=annotation['onset'])
-        duration = pd.Timedelta(seconds=annotation['duration'])
-        annot_list.append({
-            'start': data.index.min() + onset,
-            'end': data.index.min() + onset + duration,
-            'channel': None,
-            'description': annotation['description']
-        })
-
-    annotations = pd.DataFrame(
-        annot_list, columns=['start', 'end', 'channel', 'description'])
-
-    return Recording(data, meta=meta, annotations=annotations)
+from ._mne import _recording_from_mne_raw
 
 
 def read_edf(path):
