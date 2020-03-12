efacc81b7b9982322a89ef9f6336528ae18c71f6
Matteo <me@mattbit.com>
2020-03-12 11:49:11 +0100

    Add Gaussian decomposition tests

---
+++ tests/analysis/test_gaussian_decomposition.py
@@ -0,0 +1,75 @@
+import numpy as np
+from unittest import TestCase
+
+from neurokit.analysis.gaussian_decomposition import (GaussianDecomposition,
+                                                      sum_of_gaussians)
+
+
+class TestGaussianDecomposition(TestCase):
+    def test_sum_of_gaussians(self):
+        xs = np.arange(100)
+
+        ys = sum_of_gaussians(xs, [(10, 30, 5)])
+        self.assertEqual(ys[30], 10)
+        self.assertAlmostEqual(ys[70], 0, places=6)
+
+        ys = sum_of_gaussians(xs, [(10, 30, 5), (20, 70, 3)])
+        self.assertAlmostEqual(ys[30], 10, places=6)
+        self.assertAlmostEqual(ys[70], 20, places=6)
+
+    def test_initial_guess(self):
+        xs = np.arange(100)
+        ys = sum_of_gaussians(xs, [(10, 30, 5), (20, 70, 3)])
+
+        dec = GaussianDecomposition(alpha=1, min_distance=5)
+        guessed_components = dec._guess_initial(ys)
+
+        self.assertEqual(len(guessed_components), 2)
+
+        a1, μ1, σ1 = guessed_components[0]
+        self.assertAlmostEqual(a1, 10., delta=1.5)
+        self.assertAlmostEqual(μ1, 30., delta=1)
+        self.assertAlmostEqual(σ1, 5., delta=1)
+
+        a0, μ0, σ0 = guessed_components[1]
+        self.assertAlmostEqual(a0, 20., delta=1.5)
+        self.assertAlmostEqual(μ0, 70., delta=1)
+        self.assertAlmostEqual(σ0, 3., delta=1)
+
+    def test_fit(self):
+        xs = np.arange(100)
+        ys = sum_of_gaussians(xs, [(10, 30, 5), (20, 70, 3)])
+
+        dec = GaussianDecomposition(alpha=1, min_distance=5)
+        dec.fit(ys)
+
+        self.assertEqual(dec.n_components_, 2)
+
+        a0, μ0, σ0 = dec.components_[0]
+        self.assertAlmostEqual(a0, 20., places=3)
+        self.assertAlmostEqual(μ0, 70., places=3)
+        self.assertAlmostEqual(σ0, 3., places=3)
+
+        a1, μ1, σ1 = dec.components_[1]
+        self.assertAlmostEqual(a1, 10., places=3)
+        self.assertAlmostEqual(μ1, 30., places=3)
+        self.assertAlmostEqual(σ1, 5., places=3)
+
+        dec = GaussianDecomposition(alpha=1, min_distance=100)
+        dec.fit(ys)
+
+        self.assertEqual(dec.n_components_, 1)
+
+        a0, μ0, σ0 = dec.components_[0]
+        self.assertAlmostEqual(a0, 20., places=3)
+        self.assertAlmostEqual(μ0, 70., places=3)
+        self.assertAlmostEqual(σ0, 3., places=3)
+
+        # Add some noise
+        dec = GaussianDecomposition(alpha=5, min_distance=10)
+        dec.fit(ys + np.random.normal(0, 0.1, size=ys.size))
+
+        a0, μ0, σ0 = dec.components_[0]
+        self.assertAlmostEqual(a0, 20., places=1)
+        self.assertAlmostEqual(μ0, 70., places=1)
+        self.assertAlmostEqual(σ0, 3., places=1)
