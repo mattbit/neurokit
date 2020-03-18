f5b091b8821f656a07000ecbdc9c4e5c7461a8c5
pmath2012 <pmath2012@live.com>
2020-03-18 19:34:19 +0100

    Initial Commit for IES detection

--- neurokit/analysis/ies_extractor.py
+++ neurokit/analysis/ies_extractor.py
@@ -1,34 +1,34 @@
-import pandas as pd
+""" This module calculates the IES Suppressions by taking average
+    of the frontal electrodes """
+
 import math
 from scipy import ndimage as ndi
 import numpy as np
 
-
-def extract(signal, channels, threshold = 8):
+def extract(signal, channels, threshold=8):
     """ Extract the IES from the EEG
 
     Parameters
     ----------
-    signal : Recording 
+    signal : neurokit.io.Recording
         The merged signal information in the form of a Recording
     channles : array
-		The channles to consider while calculating IES 
+		The channles to consider while calculating IES
     threshold: int, optional
 		The threshold in μ Volts
-    
 
     Returns
     ----------
-    ies : Recording
+    ies : neurokit.io.Recording
         The extracted durations of Iso-Electric suppressions in the  EEG
         defined as |(Fp1+Fp2)/2| < 8 μV
         The data has an additional IES column indicating whether the pulse is
         IES or not
     """
     mean_signal = np.absolute(signal.data.loc[:, channels].values.mean(axis=1))
-    mean_signal = mean_signal < threshold 
-    possible_ies = ndi.morphology.binary_opening(mean_signal, structure =
-                                                 np.ones(math.ceil(signal.frequency)))
-    signal.data['IES'] = possible_ies
+    possible_ies = mean_signal < threshold
+    ies_mask = ndi.morphology.binary_opening(possible_ies, structure=
+                                             np.ones(math.ceil(signal.frequency)))
+    signal.data['IES'] = ies_mask
     return signal
     
