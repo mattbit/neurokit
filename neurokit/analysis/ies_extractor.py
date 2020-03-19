0729ab9286c15d938b86f1a3ba2f2a23a4298ebe
pmath2012 <pmath2012@live.com>
2020-03-19 13:10:02 +0100

    Fixing issues

--- neurokit/analysis/ies_extractor.py
+++ neurokit/analysis/ies_extractor.py
@@ -1,34 +1,35 @@
-""" This module calculates the IES Suppressions by taking average
+"""This module calculates the IES Suppression by taking average
     of the frontal electrodes """
 
 import math
 from scipy import ndimage as ndi
 import numpy as np
 
-def extract(signal, channels, threshold=8):
-    """ Extract the IES from the EEG
+
+def extract(recording, channels, threshold=8):
+    """Extract the IES from the EEG
 
     Parameters
     ----------
-    signal : neurokit.io.Recording
-        The merged signal information in the form of a Recording
-    channles : array
-		The channles to consider while calculating IES
-    threshold: int, optional
-		The threshold in μ Volts
+    recording : neurokit.io.Recording
+        The merged recording information in the form of a Recording
+    channels : numpy.ndarray
+        The channels to consider while calculating IES
+    threshold: float, optional
+        The threshold in μV
 
     Returns
     ----------
     ies : neurokit.io.Recording
-        The extracted durations of Iso-Electric suppressions in the  EEG
+        The extracted durations of Iso-Electric suppression in the  EEG
         defined as |(Fp1+Fp2)/2| < 8 μV
         The data has an additional IES column indicating whether the pulse is
         IES or not
     """
-    mean_signal = np.absolute(signal.data.loc[:, channels].values.mean(axis=1))
-    possible_ies = mean_signal < threshold
-    ies_mask = ndi.morphology.binary_opening(possible_ies, structure=
-                                             np.ones(math.ceil(signal.frequency)))
-    signal.data['IES'] = ies_mask
-    return signal
-    
+    mean_recording = np.absolute(recording.data.loc[:, channels].values.mean(axis=1))
+    possible_ies = mean_recording < threshold
+    ies_mask = ndi.morphology.binary_opening(possible_ies,
+                                             structure=np.ones(math.ceil(recording.frequency)))
+    ies = recording.copy()
+    ies.data['IES'] = ies_mask
+    return ies
