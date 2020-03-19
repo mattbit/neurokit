757bcea5ce54b1ba44ae8e0462ecf2c95078124d
Matteo <me@mattbit.com>
2020-03-19 16:12:15 +0100

    Updated implementation of IES detection

---
+++ neurokit/analysis/suppressions.py
@@ -0,0 +1,51 @@
+"""
+This module calculates the IES Suppression by taking average of the frontal
+electrodes.
+"""
+
+import math
+import numpy as np
+import pandas as pd
+from scipy.ndimage.morphology import binary_opening
+
+from ..io import Recording
+from ..utils import mask_to_intervals
+
+
+def detect_ies(recording: Recording, channels=None, threshold=8.,
+               min_duration=1.):
+    """Extract the IES from a Recording.
+
+    @todo See: the paper
+
+    Parameters
+    ----------
+    recording : neurokit.io.Recording
+        The merged recording information in the form of a Recording.
+    channels : collection.abc.Sequence
+        The channels to consider while calculating IES.
+    threshold: float, optional
+        The threshold value.
+    min_duration: float, optional
+        Minimum duration of a suppression in seconds.
+
+    Returns
+    -------
+    detections : pandas.DataFrame
+        The extracted detections of iso-electric suppressions.
+    """
+    if not channels:
+        channels = recording.channels
+
+    avg = recording.data.loc[:, channels].values.mean(axis=1)
+    min_length = math.ceil(min_duration * recording.frequency)
+    ies_mask = binary_opening(np.abs(avg) < threshold, np.ones(min_length))
+
+    intervals = mask_to_intervals(ies_mask, recording.data.index)
+    detections = [{'start': start,
+                   'end': end,
+                   'channel': None,
+                   'description': 'IES'}
+                  for start, end in intervals]
+
+    return pd.DataFrame(detections)
