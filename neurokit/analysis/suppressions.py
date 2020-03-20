a2b950a741dfd0f258eb454865505ee22de1ed31
pmath2012 <pmath2012@live.com>
2020-03-20 16:31:10 +0100

    Initial Commit

--- neurokit/analysis/suppressions.py
+++ neurokit/analysis/suppressions.py
@@ -6,6 +6,7 @@ electrodes.
 import math
 import numpy as np
 import pandas as pd
+import scipy.signal as ss
 from scipy.ndimage.morphology import binary_opening
 
 from ..io import Recording
@@ -49,3 +50,47 @@ def detect_ies(recording: Recording, channels=None, threshold=8.,
                   for start, end in intervals]
 
     return pd.DataFrame(detections)
+
+
+def detect_alpha_suppression(recording: Recording, channels=None, frequency_band=None):
+    """Extract Alpha Suppression from recording
+    Parameters
+    ----------
+    recording: neurokit.io.Recording
+        The merged recording information in the form of a Recording
+    channels: collection.abc.Sequence
+        The channels to consider while calculating alpha suppressions
+    frequency_band:  collection.abc.Sequence
+        The frequency band to preserve for filtering in the form of [minFrequency, maxFrequency]
+
+    Returns
+    -------
+    detections : pandas.DataFrame
+        the extracted durations of alpha suppression
+    """
+    if not channels:
+        channels = recording.channels
+    filtered_signals = recording.data.loc[:, channels].apply(lambda x: _do_filter(x,
+                                                                                  recording.frequency,
+                                                                                  frequency_band))
+
+
+def _do_filter(signal, fs, frequency_band, order=4):
+    """Filter the signal in the given frequency band using a butterworth filter
+
+    Parameters
+    ----------
+    signal : numpy.array
+        signal to be filtered
+    fs: float
+        sampling frequency of the recording
+    frequency_band:
+        desired frequency range
+
+    Returns
+    -------
+        filtered : numpy.array
+            filtered signal
+    """
+    sos = ss.butter(order, frequency_band, 'bp', fs=fs, output='sos')
+    return ss.sosfilt(sos, signal)
