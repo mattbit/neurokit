7bd0767d06859d2053faa922567948193b12b19e
pmath2012 <pmath2012@live.com>
2020-03-24 19:33:31 +0100

    Initial implementation of alpha suppressions (#15)

--- neurokit/analysis/suppressions.py
+++ neurokit/analysis/suppressions.py
@@ -10,6 +10,7 @@ from scipy.ndimage.morphology import binary_opening
 
 from ..io import Recording
 from ..utils import mask_to_intervals
+from ..preprocessing.filter import bandpass
 
 
 def detect_ies(recording: Recording, channels=None, threshold=8.,
@@ -39,7 +40,8 @@ def detect_ies(recording: Recording, channels=None, threshold=8.,
 
     avg = recording.data.loc[:, channels].values.mean(axis=1)
     min_length = math.ceil(min_duration * recording.frequency)
-    ies_mask = binary_opening(np.abs(avg) < threshold, np.ones(min_length))
+    th = threshold
+    ies_mask = binary_opening(np.abs(avg) < th, np.ones(min_length))
 
     intervals = mask_to_intervals(ies_mask, recording.data.index)
     detections = [{'start': start,
@@ -49,3 +51,31 @@ def detect_ies(recording: Recording, channels=None, threshold=8.,
                   for start, end in intervals]
 
     return pd.DataFrame(detections)
+
+
+def detect_alpha_suppressions(recording: Recording, channels=None, frequency_band=(8, 16)):
+    """Extract Alpha Suppression from recording
+    Parameters
+    ----------
+    recording: neurokit.io.Recording
+        The merged recording information in the form of a Recording
+    channels: collection.abc.Sequence
+        The channels to consider while calculating alpha suppressions
+    frequency_band:  collection.abc.Sequence
+        The frequency band to preserve for filtering in the form of [minFrequency, maxFrequency]
+
+    Returns
+    -------
+    detections : pandas.DataFrame
+        the extracted durations of alpha suppression
+    """
+    if not channels:
+        channels = recording.channels
+    rec = recording.copy()
+    rec.data = recording.data.loc[:, channels]
+    filtered = bandpass(rec, frequency_band)
+    rms_before = np.sqrt(np.mean(rec.data.loc[:, :].values**2))
+    rms_after = np.sqrt(np.mean(filtered.data.loc[:, :].values**2))
+    r = rms_after / rms_before
+    threshold = 8 * r
+    return detect_ies(rec, threshold=threshold)
