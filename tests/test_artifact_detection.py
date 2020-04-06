9b718a7bae848466f08d5a29ff2cc8089a044c99
Matteo <me@mattbit.com>
2020-04-06 19:19:01 +0200

    Fix code style

--- tests/test_artifact_detection.py
+++ tests/test_artifact_detection.py
@@ -33,7 +33,7 @@ def test_clipping_detector():
     signal = np.zeros(100)
     clipped_mask = np.random.choice([True, False], size=100)
     signal[clipped_mask] = np.random.choice([10, 5, -10, -2],
-                                             size=clipped_mask.sum())
+                                            size=clipped_mask.sum())
 
     with mock.patch.object(ClippingDetector, 'detect_levels',
                            return_value=(-1, 1)) as mock_detect_levels:
