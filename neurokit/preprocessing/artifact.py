c00671a3b29b30bbde96f44ef72e7525e09c676c
Matteo <me@mattbit.com>
2020-04-06 19:18:25 +0200

    Clipped signal detector

--- neurokit/preprocessing/artifact.py
+++ neurokit/preprocessing/artifact.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy.ndimage import binary_opening
+from scipy.ndimage import binary_opening, binary_dilation, gaussian_filter1d
 from skimage.filters import apply_hysteresis_threshold
 
 from ..utils import mask_to_intervals
@@ -46,18 +46,67 @@ def detect_signal_artifacts(signal, detectors=None):
     return mask
 
 
-def detect_clipped_signal(signal):
-    hist, edges = np.histogram(signal[~np.isnan(signal)],
-                               bins=65536, density=True)
+class ArtifactDetector:
+    """Detects signal artifacts.
+    """
+    multi_channel = False
 
-    mask = np.ones(len(signal), dtype=bool)
-    if hist[0] > hist[:50].mean():
-        mask &= signal > edges[1]
+    def detect(self, signal: np.ndarray) -> np.ndarray:
+        raise NotImplementedError("Detection method not implemented")
 
-    if hist[-1] > hist[:-50].mean():
-        mask &= signal < edges[-2]
 
-    return ~mask
+class ClippingDetector(ArtifactDetector):
+    """Detects clipped signal, roughly based on the method described in [1]_.
+
+    Parameters
+    ----------
+    border_bins : int
+        Number of bins to consider to calculate the deviation.
+    total_bins : int
+        Number of bins used to calculate the histogram.
+
+    References
+    ----------
+    .. [1] Laguna, Christopher, and Alexander Lerch. "An efficient algorithm
+       for clipping detection and declipping audio." Audio Engineering Society
+       Convention 141 (2016).
+    """
+
+    def detect(self, signal):
+        low_clip, high_clip = self.detect_levels(signal)
+
+        mask = np.zeros(len(signal), dtype=bool)
+        if low_clip is not None:
+            mask |= signal < low_clip
+        if high_clip is not None:
+            mask |= signal > high_clip
+
+        return mask
+
+    def detect_levels(self, signal):
+        # Guess a sensible number of bins
+        valid_signal = signal[~np.isnan(signal)]
+        values = np.unique(valid_signal)
+        if values.size < 100:
+            raise Exception("Not enough unique values to detect clipping.")
+        num_bins = min(values.size, 65536)
+
+        # Calculate the histogram
+        hist, edges = np.histogram(valid_signal, bins=num_bins)
+
+        # Detect the clipping
+        sigma = int(np.round(0.01 * num_bins))
+        a, b = 3 * sigma, 10 * sigma
+        ref = hist - gaussian_filter1d(hist, sigma, mode='constant')
+
+        _threshold_low = ref[a:b].mean() + 3 * ref[a:b].std()
+        _threshold_high = ref[-b:-a].mean() + 3 * ref[-b:-a].std()
+
+        low_level = edges[1] if ref[0] > _threshold_low else None
+        high_level = edges[-2] if ref[-1] > _threshold_high else None
+
+        return low_level, high_level
+
 
 
 def detect_isoelectrical_signal(signal, tol=0, opening=10):
---
+++ tests/test_artifact_detection.py
@@ -0,0 +1,44 @@
+import numpy as np
+from pytest import approx
+from unittest import mock
+
+from neurokit.preprocessing import ClippingDetector
+
+
+def test_clipping_levels():
+    signal = np.sin(np.arange(1000)) + np.random.normal(0, 0.1, size=1000)
+    det = ClippingDetector()
+
+    # No clipping
+    low, high = det.detect_levels(signal)
+    assert low is None
+    assert high is None
+
+    # High clipping
+    signal = signal.clip(-np.inf, 0.88)
+    low, high = det.detect_levels(signal)
+
+    assert low is None
+    assert high == approx(0.88, rel=.005)
+
+    # High and low clipping
+    signal = signal.clip(-0.6, 0.88)
+    low, high = det.detect_levels(signal)
+
+    assert low == approx(-0.6, rel=.005)
+    assert high == approx(0.88, rel=.005)
+
+
+def test_clipping_detector():
+    signal = np.zeros(100)
+    clipped_mask = np.random.choice([True, False], size=100)
+    signal[clipped_mask] = np.random.choice([10, 5, -10, -2],
+                                             size=clipped_mask.sum())
+
+    with mock.patch.object(ClippingDetector, 'detect_levels',
+                           return_value=(-1, 1)) as mock_detect_levels:
+        det = ClippingDetector()
+        detected_mask = det.detect(signal)
+
+    mock_detect_levels.assert_called_once_with(signal)
+    assert (detected_mask == clipped_mask).all()
