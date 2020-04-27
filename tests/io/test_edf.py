59a030b607bb434859d5d35d7bba9c042b461ed5
Matteo <me@mattbit.com>
2020-04-27 20:19:01 +0200

    Move and fix `io` test

--- tests/io/test_edf.py
+++ tests/io/test_edf.py
@@ -1,10 +1,7 @@
-from unittest import TestCase
-from pytest import approx
 from neurokit.io import edf
-import mne
 
 
-def test_read_edf(self):
+def test_read_edf():
     rec = edf.read_edf('tests/data/test.edf')
     assert rec.data.frequency == 100
     assert len(rec.data.channels) == 1
