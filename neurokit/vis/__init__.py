49e8933f8aba5a83b7426f60552736f8fdcccd3f
Matteo Dora <matteo.dora@ens.fr>
2020-10-07 16:25:02 +0200

    Add scale option to spectrogram plot

--- neurokit/vis/__init__.py
+++ neurokit/vis/__init__.py
@@ -2,7 +2,11 @@ import numpy as np
 import plotly.graph_objs as go
 
 
-def plot_spectrogram(f, t, S, fmin=0, fmax=40, autoclip=True):
+def plot_spectrogram(f, t, S, fmin=0, fmax=40, autoclip=True, scale='db'):
+    scale = scale.lower() if scale is not None else 'linear'
+    if scale not in ['db', 'linear']:
+        raise ValueError('Invalid scale: allowed values are `db` or `linear`')
+
     S = S[(f >= fmin) & (f <= fmax)]
     freqs = f[(f >= fmin) & (f <= fmax)]
 
@@ -11,6 +15,9 @@ def plot_spectrogram(f, t, S, fmin=0, fmax=40, autoclip=True):
         zmax = np.nanquantile(S, 0.99)
         S = S.clip(zmin, zmax)
 
-    return go.Figure(data=go.Heatmap(x=t, y=freqs, z=20 * np.log10(S),
+    if scale.lower() == 'db':
+        S = np.log10(S)
+
+    return go.Figure(data=go.Heatmap(x=t, y=freqs, z=S,
                                      colorscale='spectral',
                                      reversescale=True, showscale=False))
